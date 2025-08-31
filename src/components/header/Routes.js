7684eb3f91624a11fe1b54d0ad87e65d8f40f7f9
Batgere <BatgerelBatgerel575@gmail.com>
2025-08-31 16:22:41 +0800

    first commit

---
+++ src/components/header/Routes.js
@@ -0,0 +1,44 @@
+import { color } from 'framer-motion';
+import {
+    FaHome,
+    FaDoorClosed,
+    FaFileInvoice
+} from 'react-icons/fa';
+import {
+    MdTableRestaurant,
+    MdBorderColor,
+    MdLogin
+} from "react-icons/md";
+
+export const routes = [
+    {
+      title: 'Home',
+      href: '/',
+      Icon: FaHome,
+    },
+    {
+      title: 'About',
+      href: '/about',
+      Icon: FaDoorClosed,
+    },
+    {
+      title: 'Menu',
+      href: '/menu',
+      Icon: FaFileInvoice,
+    },
+    {
+      title: 'Reservation',
+      href: '/reservation',
+      Icon: MdTableRestaurant,
+    },
+    {
+        title: 'Order Online',
+        href: '/order-online',
+        Icon: MdBorderColor,
+      },
+      {
+        title: 'Login',
+        href: '/login',
+        Icon: MdLogin,
+      },
+  ];
---
+++ src/pages/Homepage/home.js
@@ -0,0 +1,21 @@
+import './Home.css';
+
+import About from './About';
+import Hero from './Hero';
+import Highlights from './Highlights';
+import Testimonials from './Testimonials';
+
+function Main() {
+    return (
+        <main className="App">
+            <article>
+                <Hero />
+                <Highlights />
+                <Testimonials />
+                <About />
+            </article>
+        </main>
+    );
+}
+
+export default Main;
